366cd4b1203f212bfb585f4e196f66877dcddad4
Tobias Schweizer <tobias-schweizer@gmx.de>
2023-04-19 12:31:50 +0200

    feature/add_form

---
+++ components/ProductForm/index.js
@@ -0,0 +1,59 @@
+import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
+import { StyledButton } from "../Button/Button.styled";
+import useSWRMutation from "swr/mutation";
+
+async function sendRequest(url, {arg}){
+  const response = await fetch(url,{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      },
+    body: JSON.stringify(arg),
+    });
+    if(!response.ok){
+      console.error(error.status)
+    }
+}
+
+export default function ProductForm() {
+  const {trigger} = useSWRMutation("/api/products", sendRequest)
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+    trigger(productData);
+
+    event.target.reset()
+  }
+
+
+
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledHeading>Add a new Fish</StyledHeading>
+      <StyledLabel htmlFor="name">
+        Name:
+        <input type="text" id="name" name="name" />
+      </StyledLabel>
+      <StyledLabel htmlFor="description">
+        Description:
+        <input type="text" id="description" name="description" />
+      </StyledLabel>
+      <StyledLabel htmlFor="price">
+        Price:
+        <input type="number" id="price" name="price" min="0" />
+      </StyledLabel>
+      <StyledLabel htmlFor="currency">
+        Currency:
+        <select id="currency" name="currency">
+          <option value="EUR">EUR</option>
+          <option value="USD">USD</option>
+          <option value="GBP">GBP</option>
+        </select>
+      </StyledLabel>
+      <StyledButton type="submit">Submit</StyledButton>
+    </StyledForm>
+  );
+}
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -2,12 +2,24 @@ import dbConnect from "../../../db/connect.js";
 import Product from "../../../db/models/products.js"
 
 export default async function handler(request, response) {
+
   await dbConnect();
 
   if (request.method === "GET") {
     const products = await Product.find();
     return response.status(200).json(products);
-  } else {
-    return response.status(405).json({ message: "Method not allowed" });
+  } 
+
+
+  if (request.method === "POST") {
+  try {
+    const productData = request.body;
+    await Product.create(productData);
+
+    response.status(201).json({ status: "Product created" });
+  } catch (error) {
+    console.log(error);
+    response.status(400).json({ error: error.message });
   }
+}
 }
